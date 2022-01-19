75b43d6ccfc6000c368b83854536494a9e5cf5b2
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-19 16:32:48 -0500

    created turtle

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.bgcolor("black")
+
+player = Player()
 
 game_is_on = True
 while game_is_on:
